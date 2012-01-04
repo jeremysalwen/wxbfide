99aa562170f2db67a5fca051e3812074beee118a
jeremysalwen@gmail.com <jeremysalwen@gmail.com@3c0fe916-c349-0410-8c2c-950304d8a31f>
2012-01-04 08:09:29 +0000

    Fix up project, remove embedded wxscintilla
    git-svn-id: https://wxbfide.googlecode.com/svn@15 3c0fe916-c349-0410-8c2c-950304d8a31f

--- trunk/bf_ideApp.cpp
+++ trunk/bf_ideApp.cpp
@@ -10,7 +10,6 @@
 #include "bf_ideApp.h"
 
 //(*AppHeaders
-#include "NewFrame.h"
 #include <wx/image.h>
 //*)
 #include <iostream>
@@ -21,12 +20,6 @@ bool bf_ideApp::OnInit()
     //(*AppInitialize
     bool wxsOK = true;
     wxInitAllImageHandlers();
-    if ( wxsOK )
-    {
-    	NewFrame* Frame = new NewFrame(0);
-    	Frame->Show();
-    	SetTopWindow(Frame);
-    }
     //*)
     return wxsOK;
 
